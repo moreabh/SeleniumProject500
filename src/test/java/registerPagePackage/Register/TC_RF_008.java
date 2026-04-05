3b82d20b67d0a51ce369240eb636e5b46d30def1
moreabh <moreabhishek79529@gmail.com>
2026-04-05 16:04:31 +0530

    TC_RF_008

---
+++ src/test/java/registerPagePackage/Register/TC_RF_008.java
@@ -0,0 +1,53 @@
+package registerPagePackage.Register;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_RF_008 {
+
+	@Test
+	public void registerPasswordFieldVerification() throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//a[@title='My Account']")).click();
+		driver.findElement(By.xpath("//a[text()='Register']")).click();
+		driver.findElement(By.id("input-firstname")).sendKeys("auanish");
+		driver.findElement(By.id("input-lastname")).sendKeys("honur");
+		driver.findElement(By.id("input-email")).sendKeys(generateNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+		driver.findElement(By.id("input-password")).sendKeys("123454");
+		driver.findElement(By.id("input-confirm")).sendKeys("abcde");
+		driver.findElement(By.xpath("//input[@name='agree']")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		Thread.sleep(2000);
+		
+		//verification
+		
+		String expectedWarning = "Password confirmation does not match password!";
+		
+		assertEquals(driver.findElement(By.xpath("//div[text()='Password confirmation does not match password!']")).getText(),expectedWarning);
+	
+		driver.quit();
+	}
+	
+	public String generateNewEmail() {
+		
+		LocalDateTime date = LocalDateTime.now();
+		String dateToString = date.toString();
+		String dateNoColon = dateToString.replace(":", "");
+		String dateNoHyphen = dateNoColon.replace("-", "");
+		String dateFinal = dateNoHyphen+"@gmail.com";
+		return dateFinal;
+	}
+}
